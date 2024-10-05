d6fa911c0c06fe34f91acdc21885291c1fa7cc34
Khaled Abo <36343726+Khaled85abo@users.noreply.github.com>
2024-10-05 21:48:29 +0200

    Add detecting Api calls

---
+++ src/redux/features/detect/detectApi.ts
@@ -0,0 +1,68 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import config from "../../../config";
+import { RootState } from "../../store";
+
+export const detectApi = createApi({
+    reducerPath: "detectApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${config.BACKEND_URL}/v1/detection`,
+        prepareHeaders: (headers, { getState }) => {
+            // Get the token from the state
+            const token = (getState() as RootState).auth.token;
+            // If there is a token, add it to the headers
+            if (token) {
+                headers.set("authorization", `bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        OCR: builder.mutation({
+            query: (image) => ({
+                url: "/ocr",
+                method: "POST",
+                body: image,
+            }),
+        }),
+        OCR_light: builder.mutation({
+            query: (image) => ({
+                url: "/ocr-light",
+                method: "POST",
+                body: image,
+            }),
+        }),
+        detect8n_img: builder.mutation({
+            query: (newItemData) => ({
+                url: "/detect-8x-img",
+                method: "POST",
+                body: newItemData,
+            }),
+        }),
+        detect8x_img: builder.mutation({
+            query: (newItemData) => ({
+                url: "/detect-8x-img",
+                method: "POST",
+                body: newItemData,
+            }),
+        }),
+        classify_img: builder.mutation({
+            query: (newItemData) => ({
+                url: "/classify",
+                method: "POST",
+                body: newItemData,
+            }),
+        }),
+
+
+    }),
+});
+
+export const {
+    useOCRMutation,
+    useOCR_lightMutation,
+    useDetect8n_imgMutation,
+    useDetect8x_imgMutation,
+    useClassify_imgMutation,
+} = detectApi;
+
+
