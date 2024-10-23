bc5cc614f986f37d0956887efc9dbb3cb8e7348d
Khaled Abo <36343726+Khaled85abo@users.noreply.github.com>
2024-10-23 10:22:01 +0200

    Fix resouces delete update issue

--- src/redux/features/resource/resourceSlice.ts
+++ src/redux/features/resource/resourceSlice.ts
@@ -11,9 +11,11 @@ export const resourceSlice = createSlice({
     name: "resources",
     initialState,
     reducers: {
+        // This is not being used anymore, the resources are being fetched from the workspaceApi
         setWorkspaceResources: (state, action: PayloadAction<Resource[]>) => {
             const resources = action.payload;
             console.log("resources slice, recieved resources: ", resources);
+            // This has a major issue as the last element being deleted from a workspace, the list will not be updated to [] as there is no resource to get the workspaceId from
             if (resources.length > 0) {
                 console.log("Adding resources to state");
                 const workspaceId = resources[0].work_space_id
